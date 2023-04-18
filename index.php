8f9d922f304187df9872da5af66ef2729eb4978a
sollendy <edlannino@tutanota.com>
2023-04-18 16:18:05 +0200

    esercizio base completato(?)

--- index.php
+++ index.php
@@ -54,8 +54,48 @@
     <div class="hotel-appiccicati">
         <?php var_dump($hotels) ?>
     </div>
-    <div class="hotel-ordinati">
+
+    <div class="hotel-ordinati border border-1 w-75 m-auto">
+        <?php 
+        foreach ($hotels as $alberghi){
+        ?>
+    <table class="table">
+  <thead>
+    <tr class="border border-1">
+        <?php 
+            foreach ($alberghi as $comp => $dato){
+        ?>
+        <th class="border border-1" scope="col"><?php echo $comp ?></th>
+        <?php } ?>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+    <!-- <?php 
+            foreach ($alberghi as $comp => $dato){
+        ?>
         
+        <?php } ?> -->
+        <th class="border border-1" scope="col"><?php echo $alberghi['name'] ?></th>
+        <td class="border border-1"><?php echo $alberghi['description'] ?></td>
+        <td class="border border-1"><?php echo $alberghi['parking'] ?></td>
+        <td class="border border-1"><?php echo $alberghi['vote'] ?></td>
+        <td class="border border-1"><?php echo $alberghi['distance_to_center'] ?></td>
+    </tr>
+    <!-- <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr> -->
+  </tbody>
+</table>
+        <?php } ?>
     </div>
 </main>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
